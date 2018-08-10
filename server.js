db15c0b8bed706930b6046642a6986ff09c787fe
Nick Woodward <nickwoodward5@gmail.com>
2018-08-09 19:36:05 -0700

    trying to connect to heroku...

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ var cheerio = require("cheerio");
 // Require all models
 var db = require("./models");
 
-var PORT = process.env.MONGODB_URI || 3000;
+var PORT = process.env.PORT || 3000;
 
 // Initialize Express
 var app = express();
@@ -28,7 +28,12 @@ app.use(bodyParser.urlencoded({ extended: true }));
 // Use express.static to serve the public folder as a static directory
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost:27017/mongooseScraper", { useNewUrlParser: true });
+var MONGODB_URI = process.env.MONGODB_URI || ("mongodb://localhost:27017/mongooseScraper", { useNewUrlParser: true });
+
+// Set mongoose to leverage built in JavaScript ES6 Promises
+// Connect to the Mongo DB
+mongoose.Promise = Promise;
+mongoose.connect(MONGODB_URI);
 
 
 app.get("/scrape", function (req, res) {
